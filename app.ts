8da16be0e478275eb5be9e6b0001e4c18382e213
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-27 14:52:36 -0300

    aprendendo as constantes de variavel

--- app.ts
+++ app.ts
@@ -1,5 +1,11 @@
-let ola = (nome: string, sobrenome: string) => {
-    console.log ("olá " + nome + sobrenome);
-}
+let mensagem: string = "Seja bem vindo!!!"
+let temporadasFriends: number = 10
+let estudandoAngular:boolean = true
 
-ola("Safadão")
+
+let lestaFrutas:Array<string> = ['uva', 'banana', 'abacaxi']
+let lestaFrutas2:string[] = ['uva', 'banana', 'abacaxi']
+
+
+let notasProva:Array<number> = [8, 9, 10]
+let notasProva2:number[] = [8, 9, 10]
