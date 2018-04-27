0a1b3da73807d1e0966fc9c3605d024b1a22163f
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-27 16:14:16 -0300

    teste

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,8 @@
 "use strict";
-var ola = function (nome, sobrenome) {
-    console.log("olá " + nome + sobrenome);
-};
-ola("Safadão");
+var mensagem = "Seja bem vindo!!!";
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var lestaFrutas = ['uva', 'banana', 'abacaxi'];
+var lestaFrutas2 = ['uva', 'banana', 'abacaxi'];
+var notasProva = [8, 9, 10];
+var notasProva2 = [8, 9, 10];
