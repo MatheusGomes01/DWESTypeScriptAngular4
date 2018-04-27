1870a565451d9de3b1c7037b105d88921167e0c1
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-27 09:52:43 -0300

    alteração para usar o const

--- variavel.js
+++ variavel.js
@@ -1,7 +1,20 @@
-var serie = 'Friends'
+/*var serie = 'Friends'
 
 if(true){
     let serie = 'Game of Thrones'
+   console.log(serie)
+}
+
+
+console.log(serie)
+*/
+
+
+const serie = 'Friends'
+
+
+if(true){
+    const serie = 'Game of Thrones'
    console.log(serie)
 }
 
