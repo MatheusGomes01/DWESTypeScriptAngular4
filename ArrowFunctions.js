eeb3a90c20f4ba57d16b61533987a068bc09b37c
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-04-27 11:50:40 -0300

    AF de duas maneiras

--- ArrowFunctions.js
+++ ArrowFunctions.js
@@ -1,5 +1,15 @@
-var dobroDoValor = numero =>/*=> = ArrowFunction*/ {
-    return numero * 2
+var dobroDoValor = numero =>{  
+    let resultado = numero * 2
+    return resultado
 }
 
 console.log(dobroDoValor(7))
+
+/******************************** ou ********************************** */
+
+var dobroDoValor = numero =>  numero * 2
+
+
+console.log(dobroDoValor(7))
+
+/*=> = ArrowFunction*/
