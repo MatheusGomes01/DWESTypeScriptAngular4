ac1f92174238588dea026e0d20bd88dffc2ce54c
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-05-17 09:42:54 -0300

    testando a vinculação de arquivos

--- dist/app4.js
+++ dist/app4.js
@@ -1,12 +1,15 @@
 "use strict";
 exports.__esModule = true;
-var concessionarioDAO_1 = require("./concessionarioDAO");
+//import { ConcessionarioDAO } from './concessionarioDAO'
 var concessionaria_1 = require("./concessionaria");
 var pessoa_1 = require("./pessoa");
-var pessoaDAO_1 = require("./pessoaDAO");
-var dao = new concessionarioDAO_1.ConcessionarioDAO();
+//import { PessoaDao } from './pessoaDAO';
+var DAO_1 = require("./DAO");
+//let dao: ConcessionarioDAO = new ConcessionarioDAO()
 var concessionaria = new concessionaria_1["default"]('', []);
-dao.inserir(concessionaria);
-var dao2 = new pessoaDAO_1.PessoaDao();
+//let dao2: PessoaDao = new PessoaDao()
 var pessoa = new pessoa_1["default"]('', '');
-dao2.atualizar(pessoa);
+var dao3 = new DAO_1.DAO();
+var dao4 = new DAO_1.DAO();
+dao3.inserir(concessionaria);
+dao4.remover(5);
--- dist/app4.ts
+++ dist/app4.ts
@@ -1,14 +1,17 @@
-import { ConcessionarioDAO } from './concessionarioDAO'
+//import { ConcessionarioDAO } from './concessionarioDAO'
 import Concessionaria from './concessionaria'
 import Pessoa from './pessoa'
-import { PessoaDao } from './pessoaDAO';
+//import { PessoaDao } from './pessoaDAO';
+import { DAO } from './DAO';
 
-let dao: ConcessionarioDAO = new ConcessionarioDAO()
+
+//let dao: ConcessionarioDAO = new ConcessionarioDAO()
 let concessionaria = new Concessionaria('', []);
-
-dao.inserir(concessionaria);
-
-let dao2: PessoaDao = new PessoaDao()
+//let dao2: PessoaDao = new PessoaDao()
 let pessoa: Pessoa = new Pessoa ('', '');
 
-dao2.atualizar(pessoa);
+let dao3: DAO<Concessionaria> = new DAO<Concessionaria>();
+let dao4: DAO<Pessoa> = new DAO<Pessoa>();
+
+dao3.inserir(concessionaria)
+dao4.remover(5)
