87de871e299e3cb82d6a5ca3ee482629dcbb1ca4
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-07-24 18:50:53 -0300

    aprendendo entre one ou two away binding

--- Angular 4/app1/src/app/painel/painel.component.ts
+++ Angular 4/app1/src/app/painel/painel.component.ts
@@ -12,15 +12,19 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = 'Traduza a frase:'
   public frases: frase [] = FRASES
+  resposta: string
   constructor() { console.log(this.frases)}
 
   ngOnInit() {
   }
 
+  //one-way-binding permite um data bite na direção de um component ao HTML
+  //two-way-binding ação do HTML que impacta novamente no component (ciclo)
 
   //se não colocar o visualizador de visibilidade o TS lê como public
-  public atualizaResposta():void {
-    console.log('teste')
+  public atualizaResposta(resposta: Event):void {
+    this.resposta =  (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)
   }
 
 }
