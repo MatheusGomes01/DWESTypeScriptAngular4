5c8f0d27d47682912ff15121ad0d60d72b48c671
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-08-14 16:31:03 -0300

    mudando a frase a cada acerto

--- Angular 4/app1/src/app/painel/frase-mock.ts
+++ Angular 4/app1/src/app/painel/frase-mock.ts
@@ -3,7 +3,7 @@ import { frase } from '../shared/frase.model'
 
 export const FRASES: frase[] = [
     
-    { fraseEng: 'I like to lean' , frasePtBr: 'Eu gosto de Aprender'},
+    { fraseEng: 'I like to lean' , frasePtBr: 'Eu gosto de aprender'},
     { fraseEng: 'I watch tv', frasePtBr: 'Eu assisto Tb' },
     { fraseEng: 'How are you ?', frasePtBr: 'Como vai você ?'},
     { fraseEng: 'I eat bread', frasePtBr: 'Eu como pão'}
--- Angular 4/app1/src/app/painel/painel.component.ts
+++ Angular 4/app1/src/app/painel/painel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { frase } from '../shared/frase.model';
 import { FRASES } from './frase-mock';
+import { text } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-painel',
@@ -13,7 +14,15 @@ export class PainelComponent implements OnInit {
   public instrucao: string = 'Traduza a frase:'
   public frases: frase [] = FRASES
   resposta: string
-  constructor() { console.log(this.frases)}
+
+  public rodada:number = 0
+  public rodadaFrase: frase 
+
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase)
+  }
 
   ngOnInit() {
   }
@@ -24,10 +33,23 @@ export class PainelComponent implements OnInit {
   //se não colocar o visualizador de visibilidade o TS lê como public
   public atualizaResposta(resposta: Event):void {
     this.resposta =  (<HTMLInputElement>resposta.target).value
-    console.log(this.resposta)
+    //console.log(this.resposta)
   }
   
   public verificaResposta(): void {
-    //consolo
+
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert('Translate correct')
+
+      //trocar pergunta da rodada
+    this.rodada++
+
+    //atualiza o objeto de frase
+    this.rodadaFrase = this.frases[this.rodada]
+
+    }else {
+      alert('Translate not correct')
+    }
+
   }
 }
