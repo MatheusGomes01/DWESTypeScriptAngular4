a5461ccc7dc5f05119efd277bbd76e82a6ea6962
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-09-05 12:16:23 -0300

    adicionando build de prod e mudando o leanr

--- Angular 4/app1/src/app/painel/frase-mock.ts
+++ Angular 4/app1/src/app/painel/frase-mock.ts
@@ -3,7 +3,7 @@ import { frase } from '../shared/frase.model'
 
 export const FRASES: frase[] = [
     
-    { fraseEng: 'I like to lean' , frasePtBr: 'Eu gosto de aprender'},
+    { fraseEng: 'I like to learn' , frasePtBr: 'Eu gosto de aprender'},
     { fraseEng: 'I watch tv', frasePtBr: 'Eu assisto Tv' },
     { fraseEng: 'How are you ?', frasePtBr: 'Como vai você ?'},
     { fraseEng: 'I eat bread', frasePtBr: 'Eu como pão'}
