5d3ed397bdaeb328908dc7040cee5677ecfbb536
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-09-18 09:17:33 -0300

    criando os apps de links

--- Angular 4/app2/src/app/ofertas.service.ts
+++ Angular 4/app2/src/app/ofertas.service.ts
@@ -1,93 +1,22 @@
+import { Http } from '@angular/http'
+import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
 
+import  'rxjs/add/operator/toPromise'
+
+@Injectable()
 export class OfertasService{
 
+    constructor(private  http: Http){
 
-    public ofertas: Array<Oferta> = [
-            {
-                id: 1,
-                categoria: "restaurante",
-                titulo: "Super Burger",
-                descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-                anunciante: "Original Burger",
-                valor: 29.90,
-                destaque: true,
-                imagens: [
-                    { url: "/assets/ofertas/1/img1.jpg" },
-                    { url: "/assets/ofertas/1/img2.jpg" },
-                    { url: "/assets/ofertas/1/img3.jpg" },
-                    { url: "/assets/ofertas/1/img4.jpg" }
-                ]
-            },
-            {
-                id: 2,
-                categoria: "restaurante",
-                titulo: "Cozinha Mexicana",
-                descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-                anunciante: "Mexicana",
-                valor: 32.90,
-                destaque: true,
-                imagens: [
-                    { url: "/assets/ofertas/2/img1.jpg" },
-                    { url: "/assets/ofertas/2/img2.jpg" },
-                    { url: "/assets/ofertas/2/img3.jpg" },
-                    { url: "/assets/ofertas/2/img4.jpg" }
-                ]
-            },
-            {
-                id: 4,
-                categoria: "diversao",
-                titulo: "Estância das águas",
-                descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-                anunciante: "Estância das águas",
-                valor: 31.90,
-                destaque: true,
-                imagens: [
-                    { url: "/assets/ofertas/3/img1.jpg" },
-                    { url: "/assets/ofertas/3/img2.jpg" },
-                    { url: "/assets/ofertas/3/img3.jpg" },
-                    { url: "/assets/ofertas/3/img4.jpg" },
-                    { url: "/assets/ofertas/3/img5.jpg" },
-                    { url: "/assets/ofertas/3/img6.jpg" }
-                ]
-            }
-        ]
-    
-    getOfertas(): Array<Oferta>{
-        
-        return this.ofertas
     }
 
-    public getOfertas2(): Promise<Oferta[]>{
-
-        return new Promise((resolve, reject) => {
-            //algum tipo de processsamento que ao finalizar chama a funcao resolve ou reject
-            //console.log('teste')
-            let ok = true
-            
-            if(ok){
-                setTimeout(() => resolve(this.ofertas), 3000)
-            }else {
-                reject({ codigo_erro: 404, mensagem_erro: 'servidor indisponivel xpto'})
-            }
-        })
-
-        .then(( ofertas: Oferta[]) => {
-            //trazer alguma tratativa
-            console.log('1')
-            return ofertas
-        })
-
-        .then(( ofertas: Oferta[]) => {
-            console.log('2')
-            return new Promise((resolve2, reject2) => {
-                setTimeout(() => { resolve2 ( ofertas )},3000)
-            })
-        })
-
-        .then(( ofertas: Oferta[]) => {
-            console.log('3')
-            return ofertas
-        })
+    getOfertas(): Promise<Oferta[]>{
+        
+        //efetuar uma requisição http
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((resposta: any) => resposta.json())
+        //retornar uma promisse Oferta[]
     }
 }
