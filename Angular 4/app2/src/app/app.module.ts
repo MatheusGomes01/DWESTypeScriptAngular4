1d195112b06588301a2f7a8614c7f57d88af7e22
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-09-20 10:05:13 -0300

    fazendo os property binding e adicionando pasta oferta

--- Angular 4/app2/src/app/app.module.ts
+++ Angular 4/app2/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http'
+import { RouterModule } from '@angular/router'
+
+import { ROUTES } from './app.routes'
 
 
 import { AppComponent } from './app.component';
@@ -9,6 +12,7 @@ import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
+import { OfertaComponent } from './oferta/oferta.component';
 
 
 @NgModule({
@@ -18,11 +22,13 @@ import { DiversaoComponent } from './diversao/diversao.component';
     HomeComponent,
     RodapeComponent,
     RestaurantesComponent,
-    DiversaoComponent
+    DiversaoComponent,
+    OfertaComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
