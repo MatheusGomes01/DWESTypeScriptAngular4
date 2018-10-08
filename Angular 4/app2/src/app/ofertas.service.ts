de2c86354e6a421f9bad3925fd9890a4e263d30d
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2018-10-08 09:08:30 -0300

    criando diretorios e incluindo div em oferta.html

--- Angular 4/app2/src/app/ofertas.service.ts
+++ Angular 4/app2/src/app/ofertas.service.ts
@@ -1,13 +1,13 @@
 import { Http } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
+import { url_api } from './app.api'
 
 import  'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class OfertasService{
 
-    private url_api = 'http://localhost:3000/ofertas'
     constructor(private  http: Http){
 
     }
@@ -15,20 +15,20 @@ export class OfertasService{
     getOfertas(): Promise<Oferta[]>{
         
         //efetuar uma requisição http
-        return this.http.get(`${this.url_api}?destaque=true`)
+        return this.http.get(`${url_api}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
         //retornar uma promisse Oferta[]
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]>{
-        return this.http.get(`${this.url_api}?categoria=${categoria}`)
+        return this.http.get(`${url_api}?categoria=${categoria}`)
         .toPromise()
         .then((resposta:any) => resposta.json())
     }
 
     public getOfertaPorId(id: number): Promise<Oferta>{
-        return this.http.get(`${this.url_api}?id=${id}`)
+        return this.http.get(`${url_api}?id=${id}`)
         .toPromise()
         .then((resposta:any) => {
             return resposta.json()[0] /*or.shift()*/
